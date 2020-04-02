37515d595d2248357affd02525d0b45d83269502
Nandhika Prayoga <nann011530@gmail.com>
2020-04-03 01:44:41 +0700

    Update data

--- beReimburse/urls.py
+++ beReimburse/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from beReimburse import views
 
 urlpatterns = [
-    path("pisang/", views.index, name="reimburse"),
-    path("reimburse/", views.index, name="reimburse"),
+    # Change status of reimburse
+    path("reimburses/<str:reimburse_id>/accept", views.accept_reimburse, name="accept_reimburse"),
+    path("reimburses/<str:reimburse_id>/reject", views.reject_reimburse, name="reject_reimburse"),
+    path("reimburses/<str:reimburse_id>/on-progress", views.on_progress_reimburse, name="on_progress_reimburse"),
+
+    # Etc
+    path("", views.index, name="reimburse"),
 
 ]
--- beReimburse/views.py
+++ beReimburse/views.py
@@ -2,22 +2,52 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from beReimburse import models
 from django.contrib.auth.models import User
+import json
 
 def index(request):
     pisang ={
         "description": "ngentod",
     }
+
+    # Create user
     user = User(
-        username = "bambang123",
+        username = "something2",
         #password = "bbg21",
     )
     user.set_password("bbg21")
     user.save()
+
+    # Creat employee
     emp = models.Employee(
-        name = "bambang",
+        name = "somethign 2",
         nrek = "14045",
         user = user,
     )
     emp.save()
-# Create your views here.
-    return JsonResponse(emp)
+
+    # Create your views here.
+    return JsonResponse({
+        "name": emp.name,
+        "no-rek": emp.nrek,
+        "userName": emp.user.username,
+    })
+
+def accept_reimburse(request, reimburse_id):
+    reimburse = models.Reimburses.objects.get(id=reimburse_id)
+    reimburse.stat = "Accepted"
+    reimburse.save()
+
+    return JsonResponse({
+        "statusCode": 200,
+    })
+
+def reject_reimburse(request):
+    return JsonResponse({
+        "statusCode": 200,
+    })
+
+def on_progress_reimburse(request):
+    return JsonResponse({
+        "statusCode": 200,
+    })
+
